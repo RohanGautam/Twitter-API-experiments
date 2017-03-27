4f2e84b5fd6ea84ac9b82ce96f0f1f571e9d75c1
Rohan Gautam <rohan1gautam@gmail.com>
2017-03-27 23:40:11 +0530

    Update twitter_with_urlrequest.py

--- twitter_with_urlrequest.py
+++ twitter_with_urlrequest.py
@@ -6,12 +6,12 @@ import twitter
 #import json
 
 
+#enter your twitter api info below:
+con_secret = ''
+con_secret_key = ''
+token = ''
+token_key = ''
 
-con_secret = 'VKwjWAXnuX2wywUZYVsknC0oY'
-con_secret_key = 'UAeNa2QE4Nk8yuDdtvv2ykH6aLF1lat9yx1r824XUgrlx6nPwp'
-token = '2428617499-muXCuNlhiquXythOH9s8mXo4PBp8ZjiZCyX3Ly8'
-token_key = '78YMqJiibi8ooNTmDfyDDwZ7hjfT05EqSV07QgxLzVD4f'
-#search_url='https://api.twitter.com/1.1/statuses/mentions_timeline.json'
 t=twitter.Twitter(auth=twitter.OAuth(token, token_key, con_secret, con_secret_key))
 
 
@@ -23,7 +23,7 @@ class AddLocationForm(BoxLayout):
         t.statuses.update(status=self.search_input.text)
         self.search_results.item_strings.append('Tweeted successfully!!')
     def twet(self):
-        #from multiprocessing import Process
+        #using multithreading to prevent lag in the app
         from threading import Thread
         thread = Thread(target=self.status_update)
         thread.start()

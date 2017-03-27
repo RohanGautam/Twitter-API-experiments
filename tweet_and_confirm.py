7efbac646c8d18ff7edc92a27bcdc29d816f2e2e
Rohan Gautam <rohan1gautam@gmail.com>
2017-03-27 23:38:57 +0530

    Update tweet_and_confirm.py

--- tweet_and_confirm.py
+++ tweet_and_confirm.py
@@ -1,9 +1,10 @@
 import twitter
 try:
-    con_secret = 'VKwjWAXnuX2wywUZYVsknC0oY'
-    con_secret_key = 'UAeNa2QE4Nk8yuDdtvv2ykH6aLF1lat9yx1r824XUgrlx6nPwp'
-    token = '2428617499-muXCuNlhiquXythOH9s8mXo4PBp8ZjiZCyX3Ly8'
-    token_key = '78YMqJiibi8ooNTmDfyDDwZ7hjfT05EqSV07QgxLzVD4f'
+    #enter your twitter api info below:
+    con_secret = ''
+    con_secret_key = ''
+    token = ''
+    token_key = ''
     
     t=twitter.Twitter(auth=twitter.OAuth(token, token_key, con_secret, con_secret_key))
     statupdate=raw_input('Enter text that you want to tweet: ')

c0758ccc8c1539c123667610e41c5fa562dc21e6
Rohan Gautam <rohan1gautam@gmail.com>
2017-03-27 23:37:32 +0530

    Update tweet__message__view__info.py

--- tweet__message__view__info.py
+++ tweet__message__view__info.py
@@ -1,8 +1,9 @@
 import twitter
-con_secret = 'VKwjWAXnuX2wywUZYVsknC0oY'
-con_secret_key = 'UAeNa2QE4Nk8yuDdtvv2ykH6aLF1lat9yx1r824XUgrlx6nPwp'
-token = '2428617499-muXCuNlhiquXythOH9s8mXo4PBp8ZjiZCyX3Ly8'
-token_key = '78YMqJiibi8ooNTmDfyDDwZ7hjfT05EqSV07QgxLzVD4f'
+#enter your twitter api information below
+con_secret = ''
+con_secret_key = ''
+token = ''
+token_key = ''
 t=twitter.Twitter(auth=twitter.OAuth(token, token_key, con_secret, con_secret_key))
 print 'choose:\n\t1) Messaging a user, or\n\t2) Getting information about a user,or\n\t3) Viewing the "n" most recent tweets of a user,or\n\t4)Tweet something!'
 choice=raw_input()
